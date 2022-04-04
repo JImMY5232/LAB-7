44a3f4e521629a9aa72a5ac14c2dbe75fd57ed07
JImMY5232 <98219440+JImMY5232@users.noreply.github.com>
2022-04-04 13:34:02 -0400

    Update script.7.py

--- script.7.py
+++ script.7.py
@@ -8,12 +8,22 @@ def main():
             'pepperoni'
         ],
         'movies' : [
-            {'title':'Harry Potter'
-             'genre':'fantasy',
+            {'title':'Harry Potter',
+             'genre':'fantasy'
             },
-            { 'title':'John Wick'
+            { 'title':'John Wick',
               'genre':'action'
             }
         ]
     }
 
+    movie_1 = {'title':'Harry Potter','genre':'fantasy'}
+    movie_2 = {'title':'John Wick','genre':'action'}
+    personal_information['movie'].append(movie_1,movie_2)
+
+    toppings ={ 'cheese' ,'Mushrooms','pepperoni'}
+    pizza_toppings(personal_information , toppings)
+
+def print_1(personal_information):
+    sentence ='hi joe, my name is ', personal_information['name'] , 'and my student ID is' ,personal_information['student_id'],'.'
+    print(sentence)
