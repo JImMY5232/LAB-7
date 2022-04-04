91ca93dbfa2295f3eea342b2ef87de8c6eed456f
JImMY5232 <98219440+JImMY5232@users.noreply.github.com>
2022-04-04 12:59:54 -0400

    Create script.7.py

---
+++ script.7.py
@@ -0,0 +1,19 @@
+def main():
+    personal_information = {
+        'name':'Jimmy' ,
+        'student_id':'10265232',
+        'pizza_toppings': [
+            'cheese' ,
+            'Mushrooms',
+            'pepperoni'
+        ],
+        'movies' : [
+            {'title':'Harry Potter'
+             'genre':'fantasy',
+            },
+            { 'title':'John Wick'
+              'genre':'action'
+            }
+        ]
+    }
+
